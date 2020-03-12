92d27e8c08b5b58d3e0f198c0a77d2a2d7f3eb43
gorohoroh <gorohoroh@gmail.com>
2020-03-12 23:44:22 +0300

    The rest of the sample plugin based on "Extending WC-Admin Reports" (https://woocommerce.wordpress.com/2020/02/20/extending-wc-admin-reports/). I understand very little of what's going on here

--- gorohoroh-wcadmin-extension-starter-pack/gorohoroh-wcadmin-extension-starter-pack.php
+++ gorohoroh-wcadmin-extension-starter-pack/gorohoroh-wcadmin-extension-starter-pack.php
@@ -42,3 +42,81 @@ function add_extension_register_script() {
 }
 
 add_action( 'admin_enqueue_scripts', 'add_extension_register_script' );
+
+function add_currency_settings() {
+    $currencies = array(
+        array(
+            'label' => __( 'United States Dollar', 'dev-blog-example' ),
+            'value' => 'USD',
+        ),
+        array(
+            'label' => __( 'New Zealand Dollar', 'dev-blog-example' ),
+            'value' => 'NZD',
+        ),
+        array(
+            'label' => __( 'Mexican Peso', 'dev-blog-example' ),
+            'value' => 'MXN',
+        ),
+    );
+
+    $data_registry = Automattic\WooCommerce\Blocks\Package::container()->get(
+        Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry::class
+    );
+
+    $data_registry->add( 'multiCurrency', $currencies );
+}
+
+add_action( 'init', 'add_currency_settings' );
+
+function apply_currency_arg( $args ) {
+    $currency = 'USD';
+
+    if ( isset( $_GET['currency'] ) ) {
+        $currency = sanitize_text_field( wp_unslash( $_GET['currency'] ) );
+    }
+
+    $args['currency'] = $currency;
+
+    return $args;
+}
+
+add_filter( 'woocommerce_analytics_orders_query_args', 'apply_currency_arg' );
+add_filter( 'woocommerce_analytics_orders_stats_query_args', 'apply_currency_arg' );
+
+function add_join_subquery( $clauses ) {
+    global $wpdb;
+
+    $clauses[] = "JOIN {$wpdb->postmeta} currency_postmeta ON {$wpdb->prefix}wc_order_stats.order_id = currency_postmeta.post_id";
+
+    return $clauses;
+}
+
+add_filter( 'woocommerce_analytics_clauses_join_orders_subquery', 'add_join_subquery' );
+add_filter( 'woocommerce_analytics_clauses_join_orders_stats_total', 'add_join_subquery' );
+add_filter( 'woocommerce_analytics_clauses_join_orders_stats_interval', 'add_join_subquery' );
+
+function add_where_subquery( $clauses ) {
+    $currency = 'USD';
+
+    if ( isset( $_GET['currency'] ) ) {
+        $currency = sanitize_text_field( wp_unslash( $_GET['currency'] ) );
+    }
+
+    $clauses[] = "AND currency_postmeta.meta_key = '_order_currency' AND currency_postmeta.meta_value = '{$currency}'";
+
+    return $clauses;
+}
+
+add_filter( 'woocommerce_analytics_clauses_where_orders_subquery', 'add_where_subquery' );
+add_filter( 'woocommerce_analytics_clauses_where_orders_stats_total', 'add_where_subquery' );
+add_filter( 'woocommerce_analytics_clauses_where_orders_stats_interval', 'add_where_subquery' );
+
+function add_select_subquery( $clauses ) {
+    $clauses[] = ', currency_postmeta.meta_value AS currency';
+
+    return $clauses;
+}
+
+add_filter( 'woocommerce_analytics_clauses_select_orders_subquery', 'add_select_subquery' );
+add_filter( 'woocommerce_analytics_clauses_select_orders_stats_total', 'add_select_subquery' );
+add_filter( 'woocommerce_analytics_clauses_select_orders_stats_interval', 'add_select_subquery' );
--- gorohoroh-wcadmin-extension-starter-pack/src/index.js
+++ gorohoroh-wcadmin-extension-starter-pack/src/index.js
@@ -1,4 +1,58 @@
 // Import SCSS entry file so that webpack picks up changes
 import './index.scss';
 
-console.log( 'hello from gorohoroh-wcadmin-extension-starter-pack!' );
+import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+const addCurrencyFilters = ( filters ) => {
+    return [
+        {
+            label: __( 'Currency', 'dev-blog-example' ),
+            staticParams: [],
+            param: 'currency',
+            showFilters: () => true,
+            defaultValue: 'USD',
+            filters: [ ...( wcSettings.multiCurrency || [] ) ],
+        },
+        ...filters,
+    ];
+};
+
+addFilter(
+    'woocommerce_admin_orders_report_filters',
+    'dev-blog-example',
+    addCurrencyFilters
+);
+
+const addTableColumn = reportTableData => {
+    if ( 'orders' !== reportTableData.endpoint ) {
+        return reportTableData;
+    }
+
+    const newHeaders = [
+        {
+            label: 'Currency',
+            key: 'currency',
+        },
+        ...reportTableData.headers,
+    ];
+    const newRows = reportTableData.rows.map( ( row, index ) => {
+        const item = reportTableData.items.data[ index ];
+        const newRow = [
+            {
+                display: item.currency,
+                value: item.currency,
+            },
+            ...row,
+        ];
+        return newRow;
+    } );
+
+    reportTableData.headers = newHeaders;
+    reportTableData.rows = newRows;
+
+    return reportTableData;
+};
+
+addFilter( 'woocommerce_admin_report_table', 'dev-blog-example', addTableColumn );
+
