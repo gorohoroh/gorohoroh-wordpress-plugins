4456f9075cf791318178c5096b014b8b6d5113f2
gorohoroh <gorohoroh@gmail.com>
2020-03-17 14:00:53 +0300

    Frontend: dummy homegrown component as a proof of concept

--- gorohoroh-analytics-page/src/index.js
+++ gorohoroh-analytics-page/src/index.js
@@ -1,6 +1,39 @@
-// Import SCSS entry file so that webpack picks up changes
 import './index.scss';
+import {Component as ReactComponent} from '@wordpress/element';
+import {addFilter} from '@wordpress/hooks';
+import {__} from '@wordpress/i18n';
+import {Link} from "@woocommerce/components";
 
-import { addFilter } from '@wordpress/hooks';
-import { __ } from '@wordpress/i18n';
+class GorohorohSampleComponent extends ReactComponent {
+    render() {
+        return (
+            <div>This is my dummy component</div>
+        )
+    }
+}
 
+const GorohorohReport = () => {
+    return (
+        <div>
+            <GorohorohSampleComponent/>
+            <Link
+                href="edit.php?post_type=shop_coupon"
+                type="wp-admin"
+            >
+                Coupons
+            </Link>
+        </div>
+    )
+};
+
+
+addFilter('woocommerce_admin_reports_list', 'gorohoroh-analytics-page', (reports) => {
+    return [
+        ...reports,
+        {
+            report: 'gorohoroh-analytics-page', // Should this be equal to `'path' => '/analytics/gorohoroh-analytics-page'` from .php, minus the /analytics/ part?
+            title: __('Gorohoroh Analytics Page Report (title)', 'gorohoroh-analytics-page'),
+            component: GorohorohReport
+        },
+    ];
+});
