fc087757d87280667d413d76c5e2316b6cc7023a
gorohoroh <gorohoroh@gmail.com>
2020-03-17 14:48:23 +0300

    Adding a Victory chart with dummy data. Looks awful but kinda works.

--- gorohoroh-analytics-page/src/index.js
+++ gorohoroh-analytics-page/src/index.js
@@ -2,30 +2,7 @@ import './index.scss';
 import {Component as ReactComponent} from '@wordpress/element';
 import {addFilter} from '@wordpress/hooks';
 import {__} from '@wordpress/i18n';
-import {Link} from "@woocommerce/components";
-
-class GorohorohSampleComponent extends ReactComponent {
-    render() {
-        return (
-            <div>This is my dummy component</div>
-        )
-    }
-}
-
-const GorohorohReport = () => {
-    return (
-        <div>
-            <GorohorohSampleComponent/>
-            <Link
-                href="edit.php?post_type=shop_coupon"
-                type="wp-admin"
-            >
-                Coupons
-            </Link>
-        </div>
-    )
-};
-
+import {VictoryBar, VictoryChart, VictoryLabel} from 'victory';
 
 addFilter('woocommerce_admin_reports_list', 'gorohoroh-analytics-page', (reports) => {
     return [
@@ -33,7 +10,41 @@ addFilter('woocommerce_admin_reports_list', 'gorohoroh-analytics-page', (reports
         {
             report: 'gorohoroh-analytics-page', // Should this be equal to `'path' => '/analytics/gorohoroh-analytics-page'` from .php, minus the /analytics/ part?
             title: __('Gorohoroh Analytics Page Report (title)', 'gorohoroh-analytics-page'),
-            component: GorohorohReport
+            component: ValuesByCountSortedForColumns
         },
     ];
 });
+
+class ValuesByCountSortedForColumns extends ReactComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {campaignReport: {
+                "reportTitle": "Campaign Objective",
+                "reportValues": [
+                    { "key": "Conversions", "value": 132 },
+                    { "key": "Traffic", "value": 37 },
+                    { "key": "Post engagement", "value": 8 },
+                    { "key": "Reach", "value": 4 },
+                    { "key": "Video Views", "value": 2 }
+                ]
+            }}
+    }
+
+    render() {
+        return <div className="chart-container">
+            <h2>{this.state.campaignReport.reportTitle} by count</h2>
+            <VictoryChart domainPadding={10}>
+                <VictoryBar data={this.state.campaignReport.reportValues}
+                            padding={10}
+                            x="key"
+                            y="value"
+                            width={500}
+                            height={this.state.campaignReport.reportValues.length * 30}
+                            horizontal={true}
+                            labelComponent={<VictoryLabel dy={30}/>}
+                />
+            </VictoryChart>
+        </div>;
+    }
+}
