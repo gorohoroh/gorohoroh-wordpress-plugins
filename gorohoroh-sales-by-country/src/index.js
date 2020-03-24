ce55e733e349e4a9df3d6c1b8a363fe7cc992a64
gorohoroh <gorohoroh@gmail.com>
2020-03-24 16:13:41 +0300

    Sales by country: stub component

--- gorohoroh-sales-by-country/src/index.js
+++ gorohoroh-sales-by-country/src/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import {Component as ReactComponent} from '@wordpress/element';
 import {addFilter} from '@wordpress/hooks';
 import {__} from '@wordpress/i18n';
 import { } from '@woocommerce/components';
@@ -10,7 +11,41 @@ addFilter('woocommerce_admin_reports_list', 'gorohoroh-sales-by-country', (repor
         {
             report: 'gorohoroh-sales-by-country',
             title: __('Sales by Country', 'gorohoroh-sales-by-country'),
-            component: Text
+            component: CustomComponent
         },
     ];
 });
+
+class CustomComponent extends ReactComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = { data: this.getReportData() }
+    }
+
+    render() {
+        return <Chart data={this.state.data} title="Example Chart" layout="time-comparison"/>;
+    }
+
+
+    getReportData() {
+        return [
+            {
+                date: '2018-05-30T00:00:00',
+                Hoodie: {
+                    label: 'Hoodie',
+                    value: 21599,
+                },
+                Sunglasses: {
+                    label: 'Sunglasses',
+                    value: 38537,
+                },
+                Cap: {
+                    label: 'Cap',
+                    value: 106010,
+                },
+            }
+        ]
+
+    }
+}
