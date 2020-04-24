f3516874e27c82bb69e654eac2810d4879a70eb7
gorohoroh <gorohoroh@gmail.com>
2020-04-24 18:56:38 +0300

    Sample sales by country (following my own tutorial): adding mock data while live data isn't available

--- sample-sales-by-country/src/components/SalesByCountryReport/SalesByCountryReport.js
+++ sample-sales-by-country/src/components/SalesByCountryReport/SalesByCountryReport.js
@@ -1,5 +1,15 @@
 import {Component as ReactComponent} from '@wordpress/element';
+import {mockData} from '../../mockData';
 
 export class SalesByCountryReport extends ReactComponent {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: { mockData }
+        }
+    }
+
     render() { return null }
 }
---
+++ sample-sales-by-country/src/mockData.js
@@ -0,0 +1,50 @@
+export const mockData = {
+    "countries": [
+        {
+            "country": "France",
+            "country_code": "FR",
+            "sales": 33023.23,
+            "sales_percentage": 50.37,
+            "orders": 4,
+            "average_order_value": 8255.8075
+        },
+        {
+            "country": "South Korea",
+            "country_code": "KR",
+            "sales": 3760.72,
+            "sales_percentage": 5.73,
+            "orders": 1,
+            "average_order_value": 3760.72
+        },
+        {
+            "country": "Canada",
+            "country_code": "CA",
+            "sales": 1957.3,
+            "sales_percentage": 2.98,
+            "orders": 6,
+            "average_order_value": 326.27
+        },
+        {
+            "country": "Russia",
+            "country_code": "RU",
+            "sales": 607.44,
+            "sales_percentage": 0.92,
+            "orders": 3,
+            "average_order_value": 202.48
+        },
+        {
+            "country": "Croatia",
+            "country_code": "HR",
+            "sales": 26225.58,
+            "sales_percentage": 40.00,
+            "orders": 2,
+            "average_order_value": 13112.79
+        }
+    ],
+    "totals": {
+        "total_sales": 65574.27,
+        "orders": 16,
+        "countries": 5
+    },
+    "loading": false
+};
