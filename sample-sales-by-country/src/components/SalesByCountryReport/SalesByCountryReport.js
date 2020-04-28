d511473252d3131757b20b5dff8cfff3aa56abc5
gorohoroh <gorohoroh@gmail.com>
2020-04-28 22:22:35 +0300

    Sample sales by country (following my own tutorial): fetching real data

--- sample-sales-by-country/src/components/SalesByCountryReport/SalesByCountryReport.js
+++ sample-sales-by-country/src/components/SalesByCountryReport/SalesByCountryReport.js
@@ -1,7 +1,7 @@
 import {Component as ReactComponent, Fragment} from '@wordpress/element';
-import {mockData} from '../../mockData';
 import {ChartPlaceholder, ReportFilters, SummaryList, SummaryListPlaceholder, SummaryNumber, TablePlaceholder} from '@woocommerce/components';
 import {__} from '@wordpress/i18n';
+import apiFetch from '@wordpress/api-fetch';
 import {appendTimestamp, getCurrentDates, getDateParamsFromQuery, isoDateFormat} from '@woocommerce/date';
 import {default as Currency} from '@woocommerce/currency';
 import {CURRENCY as storeCurrencySetting} from '@woocommerce/settings';
@@ -18,14 +18,126 @@ export class SalesByCountryReport extends ReactComponent {
         this.state = {
             dateQuery: dateQuery,
             currency: storeCurrency,
-            data: mockData
-        }
+            allCountries: [],
+            data: { loading: true }
+        };
+
+        this.fetchData(this.state.dateQuery);
     }
 
     createDateQuery(query) {
         const {period, compare, before, after} = getDateParamsFromQuery(query);
         const {primary: primaryDate, secondary: secondaryDate} = getCurrentDates(query);
         return {period, compare, before, after, primaryDate, secondaryDate};
+    }
+
+    getQueryParameters(dateQuery) {
+        const afterDate = encodeURIComponent(appendTimestamp(dateQuery.primaryDate.after, 'start'));
+        const beforeDate = encodeURIComponent(appendTimestamp(dateQuery.primaryDate.before, 'end'));
+        return `&after=${afterDate}&before=${beforeDate}&interval=day&order=asc&per_page=100&_locale=user`;
+    }
+
+    fetchData(dateQuery) {
+
+        if(!this.state.data.loading) this.setState({data: {loading: true}});
+
+        const endPoints = {
+            'countries': '/wc/v3/data/countries?_fields=code,name',
+            'orders': '/wc-analytics/reports/orders?_fields=order_id,date_created,date_created_gmt,customer_id,total_sales',
+            'customers': '/wc-analytics/reports/customers?_fields=id,country'
+        };
+
+        const queryParameters = this.getQueryParameters(dateQuery);
+        const countriesPath = endPoints.countries;
+        const ordersPath = endPoints.orders + queryParameters;
+        const customersPath = endPoints.customers + queryParameters;
+
+        Promise.all([
+            this.state.allCountries.length === 0 ? apiFetch({path: countriesPath}) : Promise.resolve(this.state.allCountries),
+            apiFetch({path: ordersPath}),
+            apiFetch({path: customersPath})
+        ])
+            .then(([countries, orders, customers]) => {
+                const data = this.prepareData(countries, orders, customers);
+                this.setState({data: data, allCountries: countries})
+            })
+            .catch(err => console.log(err));
+    }
+
+    prepareData(countries, orders, customers) {
+        let data;
+
+        if (orders.length > 0) {
+            const ordersWithCountries = this.getOrdersWithCountries(orders, customers, countries);
+            data = this.getPerCountryData(ordersWithCountries);
+
+            data.totals = {
+                total_sales: this.getTotalNumber(data.countries, 'sales'),
+                orders: this.getTotalNumber(data.countries, 'orders'),
+                countries: data.countries.length,
+            };
+
+            data.countries = data.countries.map(country => {
+                country.sales_percentage = Math.round(country.sales / data.totals.total_sales * 10000) / 100;
+                country.average_order_value = country.sales / country.orders;
+                return country;
+            });
+        } else {
+            data = {
+                countries: [],
+                totals: {
+                    total_sales: 0,
+                    orders: 0,
+                    countries: 0
+                }
+            }
+        }
+
+        data.loading = false;
+
+        return data;
+    }
+
+    getPerCountryData(ordersWithCountries) {
+        return ordersWithCountries.reduce((accumulator, currentObject) => {
+            const countryCode = currentObject['country_code'];
+
+            if (!accumulator.countries) accumulator.countries = [];
+
+            if (!accumulator.countries.find(item => item.country_code === countryCode)) {
+                const countryObjectTemplate = {
+                    'country': currentObject['country'],
+                    'country_code': countryCode,
+                    'sales': 0,
+                    'sales_percentage': 0,
+                    'orders': 0,
+                    'average_order_value': 0
+                };
+                accumulator.countries.push(countryObjectTemplate)
+            }
+
+            const countryIndexInAccumulator = accumulator.countries.findIndex(item => item.country_code === countryCode);
+            accumulator.countries[countryIndexInAccumulator].sales += currentObject.total_sales;
+            accumulator.countries[countryIndexInAccumulator].orders++;
+
+            return accumulator;
+        }, {});
+    }
+
+    getOrdersWithCountries(orders, customers, countries) {
+        return orders.map(order => {
+            order.country_code = customers.find(item => item.id === order.customer_id).country;
+
+            const country = countries.find(item => item.code === order.country_code);
+            order.country = country ? country.name : __('Unknown country', 'wc-admin-sales-by-country');
+
+            return order;
+        });
+    }
+
+    getTotalNumber(data, property) {
+        const propertyTotal = data.reduce((accumulator, currentObject) => accumulator + currentObject[property], 0);
+        return Math.round(propertyTotal * 100) / 100;
     }
 
     render() {
@@ -47,25 +159,28 @@ export class SalesByCountryReport extends ReactComponent {
 
         const {data, currency} = this.state;
 
-        return <Fragment>
-            {reportFilters}
-            <SummaryList>
-                {() => [
-                    <SummaryNumber key='sales'
-                                   value={currency.render(data.totals.total_sales)}
-                                   label={__('Total Sales', 'sample-sales-by-country')}/>,
-                    <SummaryNumber key='countries'
-                                   value={data.totals.countries}
-                                   label={__('Countries', 'sample-sales-by-country')}/>,
-                    <SummaryNumber key='orders'
-                                   value={data.totals.orders}
-                                   label={__('Orders', 'sample-sales-by-country')}/>
-                ]}
-            </SummaryList>
-            <CountryTable countryData={data.countries}
-                          totals={data.totals}
-                          currency={currency}
-                          headers={tableHeaders}/>
-        </Fragment>
+        if (this.state.data.loading) { return <p>Waiting...</p> }
+        else {
+            return <Fragment>
+                {reportFilters}
+                <SummaryList>
+                    {() => [
+                        <SummaryNumber key='sales'
+                                       value={currency.render(data.totals.total_sales)}
+                                       label={__('Total Sales', 'sample-sales-by-country')}/>,
+                        <SummaryNumber key='countries'
+                                       value={data.totals.countries}
+                                       label={__('Countries', 'sample-sales-by-country')}/>,
+                        <SummaryNumber key='orders'
+                                       value={data.totals.orders}
+                                       label={__('Orders', 'sample-sales-by-country')}/>
+                    ]}
+                </SummaryList>
+                <CountryTable countryData={data.countries}
+                              totals={data.totals}
+                              currency={currency}
+                              headers={tableHeaders}/>
+            </Fragment>
+        }
     }
 }
